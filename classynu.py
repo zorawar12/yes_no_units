68595f0698eaaa904a5f8f9d6cb8fe3d269f6b16
Swadhin Agrawal <swadhin12a@gmail.com>
2020-08-09 17:48:12 +0530

    random_choice_from_multiple_best_choice_2020aug9

--- classynu.py
+++ classynu.py
@@ -9,8 +9,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#%%
+
 class Decision_making:
-    def __init__(self,population_size,number_of_options):
+    def __init__(self,population_size,number_of_options,x_type,h_type, m_type ,err_type):
+        self.x_type = x_type
+        self.h_type = h_type
+        self.m_type = m_type
+        self.err_type = err_type
         self.population_size = population_size
         self.number_of_options = number_of_options
         self.quorum = None
@@ -34,10 +40,10 @@ class Decision_making:
         self.ref_highest_quality = np.where(self.Dx == max(self.Dx))[0][0]
 
     def dh(self):
-        self.Dh = np.random.normal(self.mu_h,self.sigma_h,self.population_size)
+        self.Dh = np.round(np.random.normal(self.mu_h,self.sigma_h,self.population_size),decimals=self.h_type)
 
     def dx(self):
-        self.Dx = np.random.normal(self.mu_x,self.sigma_x,self.number_of_options)
+        self.Dx = np.round(np.random.normal(self.mu_x,self.sigma_x,self.number_of_options),decimals=self.x_type)
 
     def dm(self):
         """
@@ -46,7 +52,7 @@ class Decision_making:
         """
         sum = self.population_size + 1
         while sum > self.population_size:
-            Dm = np.random.normal(self.mu_m,self.sigma_m,self.number_of_options)
+            Dm = np.round(np.random.normal(self.mu_m,self.sigma_m,self.number_of_options),decimals=self.m_type)
             sum = 0
             for m in Dm:
                 sum += int(m)
@@ -73,8 +79,8 @@ class Decision_making:
         for i in range(len(self.assigned_units)):
             count = 0
             for j in self.assigned_units[i]:
-                assesment_error = np.random.normal(self.mu_assessment_err,self.sigma_assessment_err,1)
-                if j["threshold"]  < self.Dx[i] + assesment_error[0] :
+                assesment_error = np.round(np.random.normal(self.mu_assessment_err,self.sigma_assessment_err,1),decimals= self.err_type)[0]
+                if j["threshold"]  < self.Dx[i] + assesment_error :
                     count += 1
             votes.append(count)
         self.votes = votes
@@ -84,3 +90,20 @@ class Decision_making:
         for i in range(self.number_of_options):
             option_votes.append({"xi":self.Dx[i],"votes":self.votes[i]})
         self.vote_for_opt = np.array(option_votes)
+
+    def one_correct(self):
+        if np.where(self.vote_for_opt == max(self.vote_for_opt, key = lambda i: i['votes']))[0][0] == self.ref_highest_quality:
+            return 1
+
+    def multi_correct(self):
+        available_opt = np.where(self.vote_for_opt == max(self.vote_for_opt, key = lambda i: i['votes']))[0]
+        opt_choosen = np.random.randint(0,len(available_opt))
+        if available_opt[opt_choosen] ==  self.ref_highest_quality:
+            return 1
+
+#%%
+
+# class visualize:
+#     def __init__(self):
+
+
--- ynu.py
+++ ynu.py
@@ -10,41 +10,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import classynu as yn
 
+#%%
 
-
-def majority_decision():
+def majority_decision(population_size = 2000,number_of_options = 10,mu_x = 0.0,\
+    sigma_x = 1.0,mu_h = 0.0,sigma_h = 1.0,mu_m = 200,sigma_m = 0,mu_assessment_err = 0.0,\
+    sigma_assessment_err = 0.0,x_type = 3,h_type = 3, m_type = 3,err_type = 3,one_correct_opt = 1):
     """
-    Majority based decision without any assesment error
+    Majority based decision
     """
-    ynu = yn.Decision_making(population_size = 2000,number_of_options = 10)
-    ynu.mu_x = 0.0
-    ynu.sigma_x = 1.0
-    ynu.mu_h = 0.0
-    ynu.sigma_h = 1.0
-    ynu.mu_m = 200.0
-    ynu.sigma_m = 0.0
+    ynu = yn.Decision_making(population_size,number_of_options,x_type,h_type, m_type,err_type)
+    ynu.mu_x = mu_x
+    ynu.sigma_x = sigma_x
+    ynu.mu_h = mu_h
+    ynu.sigma_h = sigma_h
+    ynu.mu_m = mu_m
+    ynu.sigma_m = sigma_m
     ynu.dm()
     ynu.dh()
     ynu.dx()
     ynu.ref_highest_qual()
     ynu.units_assign_to_opt()
-    ynu.mu_assessment_err = 0.0
-    ynu.sigma_assessment_err = 0.0
+    ynu.mu_assessment_err = mu_assessment_err
+    ynu.sigma_assessment_err = sigma_assessment_err
     ynu.vote_counter()
     ynu.vote_associator()
 
-    if np.where(ynu.vote_for_opt == max(ynu.vote_for_opt, key = lambda i: i['votes']))[0][0] == ynu.ref_highest_quality:
-        print("Success")
+    if one_correct_opt == 1:
+        if ynu.one_correct() == 1:
+            print("Success")
+        else:
+            print("Failure")
+        
     else:
-        print("Failure")
+        if ynu.multi_correct() == 1:
+            print("Success")
+        else:
+            print("Failure")
+    
     plt.scatter(ynu.Dx,ynu.votes)
     plt.show()
 
+#%%
 
-majority_decision()
+majority_decision()                                   # Without assesment error
 
+#%%
 
+majority_decision(sigma_assessment_err = 5)           # With assessment error
 
+#%%
+
+majority_decision(x_type = 0, m_type = 0,err_type = 0,one_correct_opt = 0)        # Random choice when more than one option's correct
+
+#%%
 
 # num_of_units = [j for j in range(100,20000,500)]
 # num_of_opt = [j for j in range(2,20,1)]
