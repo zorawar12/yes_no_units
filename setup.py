b7c7fd055618e7a8ea5497ed5a839a9ff8c07d73
Swadhin Agrawal <swadhin12a@gmail.com>
2020-09-29 00:08:05 +0530

    setup files

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@
 
 from setuptools import setup,find_packages
 
-setup(name='Yes_no_units',version='1.0',packages=find_packages())
+setup(
+    name='Yes_no_units',
+    author='Swadhin Agrawal'
+    version='1.0',
+    packages=find_packages()
+)
